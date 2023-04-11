01ed83e6076c20564dfc86e894ea193dade36c4f
Yczar <babalolagbogo@gmail.com>
2023-04-11 15:25:55 +0100

    chore: migration and sdk constraint update

--- dart/lib/src/open_location_code.dart
+++ dart/lib/src/open_location_code.dart
@@ -105,7 +105,7 @@ bool _matchesPattern(String string, Pattern pattern) =>
     string.contains(pattern);
 
 bool isValid(String code) {
-  if (code == null || code.length == 1) {
+  if (code.length == 1) {
     return false;
   }
 
@@ -134,7 +134,7 @@ bool isValid(String code) {
     if (padMatch.length != 1) {
       return false;
     }
-    var matchLength = padMatch.first.group(0).length;
+    var matchLength = padMatch.first.group(0)?.length ?? 0;
     if (matchLength.isOdd || matchLength > separatorPosition - 2) {
       return false;
     }
